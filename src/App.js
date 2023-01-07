8c329cabb3d397bc07a0e2233253bffa18d50bb1
NikitA1827 <nikuu1999@gmail.com>
2023-01-07 13:36:54 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <h1>Nikita Sonwane</h1>
+    
+     <Navbar/>
     </div>
   );
 }
